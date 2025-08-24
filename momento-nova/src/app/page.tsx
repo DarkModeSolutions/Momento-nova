d1f425e9db1d043f24ce2ade849a49d87d9ed827
pranulkbv28 <28bvpranaav@gmail.com>
2025-08-24 23:43:20 +0530

    feat: add Wall of Love section with reviews and integrate embla-carousel for smooth navigation

--- momento-nova/src/app/page.tsx
+++ momento-nova/src/app/page.tsx
@@ -1,11 +1,13 @@
 import EventWorldSection from "@/components/EventWorldSection";
 import HeroSection from "@/components/HeroSection";
+import WallOfLoveSection from "@/components/WallOfLoveSection";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <EventWorldSection />
+      <WallOfLoveSection />
     </div>
   );
 }
