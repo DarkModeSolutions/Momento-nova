d4b1bd02b65eade2338b5e407c135b3729499545
pranulkbv28 <28bvpranaav@gmail.com>
2025-08-24 16:57:18 +0530

    feat: enhance Header component with responsive design and add MenuComponent for navigation style: update gradient styles in globals.css and add bottom gradient border class refactor: clean up imports and formatting in various UI components

--- momento-nova/src/components/Header.tsx
+++ momento-nova/src/components/Header.tsx
@@ -1,7 +1,17 @@
 "use client";
 
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { XIcon } from "lucide-react";
 import React, { useState, useEffect } from "react";
+import logoImg from "@/../public/assets/images/logo.png";
+import Image from "next/image";
+import MenuComponent from "@/components/MenuComponent";
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -20,12 +30,41 @@ const Header = () => {
 
   return (
     <div
-      className={`${isScrolled ? "glass inner-shadow" : ""} p-3 sticky top-0 z-50 transition-all duration-300 ease-in-out`}
+      className={`${isScrolled ? "glass inner-shadow" : ""} p-3 pl-10 sticky top-0 z-50 transition-all duration-300 ease-in-out flex items-center gap-6`}
     >
       <Sheet>
-        <SheetTrigger>Open</SheetTrigger>
-        <SheetContent side="left">Some Content</SheetContent>
+        <SheetTrigger
+          onClick={() => {
+            console.log("Sheet opened");
+          }}
+        >
+          <GiHamburgerMenu className="cursor-pointer" />
+        </SheetTrigger>
+        <SheetContent className="glass inner-shadow p-6 text-white" side="left">
+          <div className="flex items-center gap-20 pl-6">
+            <SheetClose>
+              <XIcon
+                className="cursor-pointer"
+                width={20}
+                height={20}
+                color="white"
+              />
+            </SheetClose>
+            <div className="">
+              <Image src={logoImg} alt="logo" width={100} height={100} />
+            </div>
+          </div>
+          <div className="mt-2 flex flex-col justify-center items-center">
+            <MenuComponent textContent="About Us" linkTo="/about-us" />
+            <MenuComponent textContent="Services" linkTo="#" />
+            <MenuComponent textContent="Portfolio" linkTo="#" />
+            <MenuComponent textContent="Blog" linkTo="#" />
+          </div>
+        </SheetContent>
       </Sheet>
+      <div className="">
+        <Image src={logoImg} alt="logo" width={75} height={50} />
+      </div>
     </div>
   );
 };
